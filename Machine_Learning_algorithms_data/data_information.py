22ca27ac8d47b06d3e08ea43c83fbc658ebf0784
Vivek Moga <vivekmoga1995@gmail.com>
2024-10-05 12:04:56 +0200

    feat: developed functions for data science, like MSD, SD, chart plotting, percentile

--- Machine_Learning_algorithms_data/data_information.py
+++ Machine_Learning_algorithms_data/data_information.py
@@ -34,7 +34,7 @@ class DataDescription():
         Arguments:
             dataset : dataset in csv format
             column_name_1: choose the column from dataset
-            column_name_2: choose the second column from the dataset for comarision
+            column_name_2: choose the second column from the dataset for comparison
 
         Return:
             changed column with numerical value
@@ -44,12 +44,70 @@ class DataDescription():
     
     def fill_na_with_mean_values_of_selected_column(self, dataset:tuple, column_name:str):
         """This function fill the null values with the mean values\n
-        Arguments:
+        Arguments:\n
               dataset : dataset in csv format
               column_name : It takes the string value, The name of the column which want to replace null values with mean      
         """
         df = pd.read_csv(self.csv_file_path)
         df[column_name].fillna(df[column_name].mean(), inplace=True)
+    
+    def get_min_and_max_value_from_data(self, target_column:str)->tuple[float, float]:
+        """
+        This function return the min and max value from the selected column\n
+        Argument:\n
+            target_column : 
+        Return:\n
+            min_value : It returns the min value from the selected column in float
+            max_value : It returns the max value from the selected column in float 
+        """
+        df = pd.read_csv(self.csv_file_path)
+        min_value = df[target_column].min()
+        max_value = df[target_column].max()
+        return min_value, max_value
+    
+    def get_mean_absolute_division_of_column_from_dataset(self, column_name_1:str)->float:
+        """
+        This function get the mean absolute division value of column for further comparison\n
+        Which ever mean value comes less select that dataset\n
+        Argument:\n
+            column_name_1: It takes the string value, choose the column from dataset
+        Return:\n
+            mean_absolute_division_first_column : It returns the float value of first column
+        """
+        df = pd.read_csv(self.csv_file_path)
+        mean_first = df[column_name_1].mean(column_name_1)
+        mean_absolute_division_first_column = df[column_name_1].sum(abs(column_name_1-mean_first))/len(column_name_1)
+        return mean_absolute_division_first_column
+    
+    def get_standard_deviation_and_variance_of_columns_from_dataset(self, column_name_1:str)->tuple[float, float]:
+        """
+        This function calculate standard deviation of column from dataset\n
+        This helpful when want to select only one dataset out of two, so that dataset can be more precise and less time consume when to predict\n
+        Take the lowest value from the SD and variance values\n
+        Argument:\n
+            column_name_1: It takes the string value, choose the column from dataset
+        Returns:\n
+            first_column_sd : It returns the float value of first column SD
+            first_column_variance : It returns the float value of first column variance
+        """
+        df = pd.read_csv(self.csv_file_path)
+        first_column_sd = df[column_name_1].std(column_name_1)
+        first_column_variance = df[column_name_1].var(column_name_1)
+        return first_column_sd, first_column_variance
+
+    def get_percentile_of_group_in_column_from_dataset(self, column_name:str, percentile_value:float)->float:
+        """
+        This function gives the percentile, means which if 50% provided then it gives its which group lies between this values\n
+        Suppose age group is lies between 20 to 80, then it gives values which group ages people are more among\n
+        Arguments:\n
+            column_name: It takes the string value, choose the column from dataset
+            percentile_value : It takes the float value (from 0 to 100)
+        Return:\n
+            percentile_range_value : It gives the which values lie in this percentile given range
+        """
+        df = pd.read_csv(self.csv_file_path)
+        percentile_range_value = np.percentile(df[column_name], percentile_value)
+        return percentile_range_value
 
 
 
--- Machine_Learning_algorithms_data/matplotlib_sns_view_interaction.py
+++ Machine_Learning_algorithms_data/matplotlib_sns_view_interaction.py
@@ -13,24 +13,65 @@ class SNSChart():
         self.csv_file_path = csv_file_path
     
     def hist_plot(self, target_column:str, min_value:int, max_value:int, offset:int, show_median_value:bool=True, show_mean_value:bool = True)->None:
-        """This function generate the chart between the range and show the mean value"""
+        """
+        This function generate the chart between the range and show the mean value\n
+        Arguments:\n
+            target_column : It takes the string value of column name
+            min_value : It takes the integer value
+            max_value : It takes the integer value
+            offset : It takes the integer value
+            show_median_value (Optional) : It takes the boolean value, if want this data then True (ByDefault) else False
+            show_mean_value (Optional) : It takes the boolean value, if want this data then True (ByDefault) else False
+        """
         dataset = pd.read_csv(self.csv_file_path)
         sns.histplot(x=target_column, data=dataset, bins=[i for i in range(min_value, max_value, offset)])
         if show_mean_value is True:
             mn = np.mean(dataset[target_column]) 
-            plt.plot([mn for i in range(0, 300)], [i for i in range(0, 300)], c="red")
+            plt.plot([mn for i in range(0, 300)], [i for i in range(0, 300)], c="red", label="mean")
             plt.show()
         if show_median_value is True:
             mn = np.median(dataset[target_column]) 
-            plt.plot([mn for i in range(0, 300)], [i for i in range(0, 300)], c="green")
+            plt.plot([mn for i in range(0, 300)], [i for i in range(0, 300)], c="green", label="median")
             plt.show()
     
     def check_mode_of_categorical_column_in_chart(self, target_column_for_data_chart:str, target_column_for_mode:str, min_value:int, max_value:int, offset:int):
-        """This function use to check the categorical data of selected column"""
+        """
+        This function use to check the categorical data of selected column
+        Arguments:\n
+            target_column_for_data_chart : It takes the string value of column name which chart want to see
+            target_column_for_mode : It takes the string value of column name which want to see mode in a chart
+            min_value : It takes the integer value
+            max_value : It takes the integer value
+            offset : It takes the integer value
+        """
         dataset = pd.read_csv(self.csv_file_path)
         mode = dataset[target_column_for_mode].mode()[0]
         sns.histplot(x=target_column_for_data_chart, data=dataset, bins=[i for i in range(min_value, max_value, offset)])
-        plt.plot([mode for i in range(0, 300)], [i for i in range(0, 300)], c="green")
-        plt.show() 
+        plt.plot([mode for i in range(0, 300)], [i for i in range(0, 300)], c="green", label="mode")
+        plt.show()
+    
+    def get_scatter_chart_with_compare_to_mean_value(self, column_name_1:str, column_name_2:str, first_name:str, second_name:str)->None:
+        """
+        This function check the scatter chart of two dataset columns\n
+        Then after check that the which values are near to the mean absolute division that column can be selected for further use\n
+        Arguments:\n
+            column_name_1: It takes the string value, choose the column from dataset
+            column_name_2: It takes the string value, choose the second column from the dataset
+            first_name : It takes the string value and provide name which will visible in chart for first selected column points
+            second_name : It takes the string value and provide name which will visible in chart for second selected column points 
+        """
+        df = pd.read_csv(self.csv_file_path)
+        row, col = df[column_name_1].shape
+        mean = df[column_name_1].mean(column_name_1)
+        plt.figure(figsize=(10, 3))
+        plt.scatter(column_name_1, label=first_name)
+        plt.scatter(column_name_2, label=second_name)
+        plt.plot([mean for i in range(1, row)], row, c="blue", label="mean")
+        plt.show()
+
+
+
+    
+
 
     
